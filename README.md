d11bd9d44a39967a6ef703a848df3bb1cb2477ef
Mohamed Shelfs <mohamedsaidlearn@gmail.com>
2021-04-10 19:48:59 +0200

    used BABEL to make js code compatable across all browsers, and update README.md file

--- README.md
+++ README.md
@@ -10,5 +10,5 @@ This is my first project on FWD initiative
 	1. Try to hover 'U' letter on Udacity name on the banner heading.
 	2. Checkout the page responsiveness.
 	3. This website is a very small - actually you can say a scratch because this is just for training - mockup for a teaching Company that teach web techs like CSS, JS.
-	
+	4. I converted the ES6 JS code that I wrote to ES5 using 'BABEL', and I saved the ES6 code in a file called ES6code in js folder.
 have a nice time ❤
--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * 
  * Made by: Mohamed Said Ahmed Mohamed Shlef
@@ -10,13 +12,12 @@
  * Define Global Variables
  * 
 */
-
 // get all the sections in the webpage by thair data attribute 
 // and convert them from node list to an array using Spread Operator
-let sections = [...document.querySelectorAll('[data-nav]')];
-//  all the sections links
-let sectionsLinks = [];
-// count number of sections
+let sections = [...document.querySelectorAll('[data-nav]')]; //  all the sections links
+
+let sectionsLinks = []; // count number of sections
+
 let sectionsNumber = sections.length;
 var fragment = document.createDocumentFragment(); // to avoid many reflows and repaints
 
@@ -26,178 +27,198 @@ var fragment = document.createDocumentFragment(); // to avoid many reflows and r
  * 
 */
 // to get the corresponding node for a single html collection
-function fromHTMLCollectionToNode(HTMLCollection){
+
+function fromHTMLCollectionToNode(HTMLCollection) {
   return [...HTMLCollection][0];
-}
-//  to detect if an element is in the view port or not
+} //  to detect if an element is in the view port or not
+
+
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+} // to remove 'your-active-class' class from all of the sections
 
-  );
-}
-// to remove 'your-active-class' class from all of the sections
-function removeActives(){
-  for(let i=0; i<sectionsNumber;i++){
+
+function removeActives() {
+  for (let i = 0; i < sectionsNumber; i++) {
     sections[i].classList.remove('your-active-class');
     sectionsLinks[i].classList.remove('active--link');
   }
-}
+} // to get corresponding section name for the targeted section link
 
-// to get corresponding section name for the targeted section link
-function correspondingSectionName(section){
-  return section.innerText.replace(/\s/g,'').toLowerCase();
-}
 
-// to return corresponding node from HTML Collection to the node
-function getNode(item){
+function correspondingSectionName(section) {
+  return section.innerText.replace(/\s/g, '').toLowerCase();
+} // to return corresponding node from HTML Collection to the node
+
+
+function getNode(item) {
   let linkName = item.textContent; //first get the link name
-  let correspondingSecHeading = document.getElementsByClassName(linkName.replace(/\s/g,'')); // get the section but this is an html collection, first we have to converst it to and array and access the first element to get it's parent
+
+  let correspondingSecHeading = document.getElementsByClassName(linkName.replace(/\s/g, '')); // get the section but this is an html collection, first we have to converst it to and array and access the first element to get it's parent
+
   return fromHTMLCollectionToNode(correspondingSecHeading); // to get the node insted of html collection
 }
-
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-
 // build the nav
-for(let i=0; i<sectionsNumber; i++){
-  let newNavItem = document.createElement("li");; // new nav link
-  let sectionHeader = sections[i].querySelector('.sectionHeader').innerText;// to save the section heading to use it later
+
+
+for (let i = 0; i < sectionsNumber; i++) {
+  let newNavItem = document.createElement("li");
+  ; // new nav link
+
+  let sectionHeader = sections[i].querySelector('.sectionHeader').innerText; // to save the section heading to use it later
+
   newNavItem.textContent = sectionHeader;
   newNavItem.classList.add('menu__link');
   sectionsLinks.push(newNavItem); // add this link to the links wrapper
+
   fragment.appendChild(newNavItem);
 }
 
 let navList = document.querySelector('#navbar__list');
-navList.appendChild(fragment);
+navList.appendChild(fragment); // Add class 'active' to section when near top of viewport
 
+document.addEventListener('scroll', function () {
+  removeActives();
 
-// Add class 'active' to section when near top of viewport
-
-document.addEventListener('scroll',function(){
-  removeActives(); 
-  for(let i=0; i<sectionsNumber ;i++){
-    if(isInViewport(sections[i])){
+  for (let i = 0; i < sectionsNumber; i++) {
+    if (isInViewport(sections[i])) {
       removeActives();
       sections[i].classList.add('your-active-class');
       sectionsLinks[i].classList.add('active--link');
     }
   }
-},{passive: true})
+}, {
+  passive: true
+}); // Scroll to anchor ID using scrollTO event
 
-// Scroll to anchor ID using scrollTO event
-for(let i=0; i<sectionsNumber; i++){
-  sectionsLinks[i].addEventListener('click',() =>{
+for (let i = 0; i < sectionsNumber; i++) {
+  sectionsLinks[i].addEventListener('click', () => {
     let correspondingNode = getNode(sectionsLinks[i]);
     let targetedSectionID = correspondingNode.parentNode.parentNode.id;
-    document.getElementById(targetedSectionID).scrollIntoView({ behavior: 'smooth' });
+    document.getElementById(targetedSectionID).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
-}
+} // hide nav bar on scrolling
 
-// hide nav bar on scrolling
-const navIsHover = e => e.parentElement.querySelector(':hover') === e;    
+
+const navIsHover = e => e.parentElement.querySelector(':hover') === e;
+
 const navBar = document.querySelector(".navbar__menu");
 
-function showNavOnScroll(){
+function showNavOnScroll() {
   let timer = null; // this is like a flag to determine if scrolling or not
-  let showNav = { "display": "block", "opacity": "1"};
-  let hideNav = { "display": "none", "opacity": "0"};
+
+  let showNav = {
+    "display": "block",
+    "opacity": "1"
+  };
+  let hideNav = {
+    "display": "none",
+    "opacity": "0"
+  };
   let navBar = document.querySelector(".navbar__menu");
   let sideNav = document.querySelector('.icon-bar');
-
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', function () {
     // collabse if not
     if (navBar.classList.contains("navbar__menu")) {
-      if(navBar.classList.contains("responsive"))
-        navBar.classList.remove("responsive");
+      if (navBar.classList.contains("responsive")) navBar.classList.remove("responsive");
+    } // function logic
+
+
+    Object.assign(navBar.style, showNav); // default state is visability
+
+    Object.assign(sideNav.style, hideNav); // default state is to be not visabil
+
+    if (timer !== null) {
+      clearTimeout(timer);
     }
-    // function logic
-    Object.assign(navBar.style,showNav); // default state is visability
-    Object.assign(sideNav.style,hideNav); // default state is to be not visabil
-    if(timer !== null) {
-      clearTimeout(timer);      
-    }
-    timer = setTimeout(function() {
+
+    timer = setTimeout(function () {
       const hovered = navIsHover(navBar);
-      if(!hovered){
-        Object.assign(navBar.style,hideNav); // default state is visability
-        Object.assign(sideNav.style,showNav); // default state is to be not visabil
+
+      if (!hovered) {
+        Object.assign(navBar.style, hideNav); // default state is visability
+
+        Object.assign(sideNav.style, showNav); // default state is to be not visabil
       }
-      }, 3000);
-    }, false);
-}
+    }, 3000);
+  }, false);
+} // scroll to top button design
 
-// scroll to top button design
-document.querySelector('.scrollTop').addEventListener('click',()=>{
-  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-});
 
-// to show toTop button while scrolling down
+document.querySelector('.scrollTop').addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+}); // to show toTop button while scrolling down
+
 var prevScrollpos = window.pageYOffset;
 document.querySelector(".scrollTop").style.display = "none"; // to hide it by default
-window.onscroll = function() {
+
+window.onscroll = function () {
   document.querySelector(".scrollTop").style.display = "block";
   var currentScrollPos = window.pageYOffset;
+
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".scrollTop").style.opacity = 0;
   } else {
     document.querySelector(".scrollTop").style.opacity = 1;
   }
-  prevScrollpos = currentScrollPos;
-}
 
-const isHover = e => e.parentElement.querySelector(':hover') === e;    
+  prevScrollpos = currentScrollPos;
+};
+
+const isHover = e => e.parentElement.querySelector(':hover') === e;
 
 const myButton = document.getElementById('scrollTop');
 document.addEventListener('mousemove', function checkHover() {
   const hovered = isHover(myButton);
+
   if (hovered !== checkHover.hovered) {
-    if(hovered){
+    if (hovered) {
       document.querySelector(".scrollTop").style.opacity = 0;
       myButton.classList.remove('fas');
       myButton.classList.add('far');
       document.querySelector(".scrollTop").style.opacity = 0.5;
-    }
-    else{
+    } else {
       document.querySelector(".scrollTop").style.opacity = 0;
       myButton.classList.remove('far');
       myButton.classList.add('fas');
       document.querySelector(".scrollTop").style.opacity = 1;
     }
+
     checkHover.hovered = hovered;
   }
-});
+}); // making the navbar responsive 
 
-// making the navbar responsive 
 function responsiveNavBar() {
   var navBar = document.querySelector(".navbar__menu");
+
   if (navBar.classList.contains("navbar__menu")) {
-    if(navBar.classList.contains("responsive"))
-      navBar.classList.remove("responsive");
-    else
-      navBar.classList.add("responsive");
+    if (navBar.classList.contains("responsive")) navBar.classList.remove("responsive");else navBar.classList.add("responsive");
   }
 }
 
-document.querySelector('#respIcon').addEventListener('click',responsiveNavBar);
+document.querySelector('#respIcon').addEventListener('click', responsiveNavBar);
 /**
  * End Main Functions
  * Begin Events
  * 
 */
 
-
 /**
  * End Events
  * Start invoking functions
  * 
  */
- showNavOnScroll();
+
+showNavOnScroll();
